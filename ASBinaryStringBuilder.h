b13d0048372b69f3bf5ad2750dfaacb05a2c21b2
Abc Dr <641503229@qq.com>
2023-04-07 08:51:34 +0800

    remove no use

--- ASBinaryStringBuilder.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-void ASBinaryStringBuilder::WriteInt(int value);
-void ASBinaryStringBuilder::WriteLong(long value);
-void ASBinaryStringBuilder::WriteFloat(float value);
-void ASBinaryStringBuilder::WriteDouble(double value);
-void ASBinaryStringBuilder::WriteVector(vec3_t value);
-void ASBinaryStringBuilder::WriteString(CString* value);
